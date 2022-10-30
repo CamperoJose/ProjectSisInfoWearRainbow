31668109c6f18af3aceae945fa201e81e0898f8d
CamperoJose <jose.campero@ucb.edu.bo>
2022-10-30 08:50:52 -0400

    actualizacion

---
+++ WearRainbow/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class persona(models.Model):
+    id_persona = models.AutoField(primary_key=True, null=False, unique=True)
+    nombre = models.CharField(max_length=30, null=False)
+    apellidoPaterno = models.CharField(max_length=30, null=False)
+    apellidoMaterno = models.CharField(max_length=30, null=False)
+    ciNumero = models.CharField(max_length=30, null=False, unique=True)
+    ciExtension = models.CharField(max_length=30, null=False)
+    ciComplemento = models.CharField(max_length=30, null=False)
+    celular = models.CharField(max_length=30, null=False)
+    correo = models.CharField(max_length=30, null=False)
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_id_persona(self):
+        return self.id_persona
+
+class administrador(models.Model):
+    id_administrador = models.AutoField(primary_key=True, null=False, unique=True)
+    id_persona = models.ForeignKey(persona, on_delete=models.CASCADE)
+    usuario  = models.CharField(max_length=30, null=False)
+    contraseña  = models.CharField(max_length=30, null=False)
+
+class cliente(models.Model):
+    id_cliente = models.AutoField(primary_key=True, null=False, unique=True)
+    id_persona = models.ForeignKey(persona, on_delete=models.CASCADE)
+    usuario  = models.CharField(max_length=30, null=False)
+    contraseña  = models.CharField(max_length=30, null=False)
---
+++ WearRainbow/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from WearRainbow.models import persona
+from WearRainbow.models import cliente
+from django.contrib import messages
+
+
+def paginaIndex(request):
+    return render(request, 'index.html')
+
+def SignInAsClient(request):
+    return render(request, 'SigninAsClient.html')
+
+def registroPersona(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        apellidoPaterno = request.POST['appP']
+        apellidoMaterno = request.POST['appM']
+        ciNumero = request.POST['CiN']
+        ciExtension = request.POST['CiE']
+        ciComplemento = request.POST['ciC']
+        celular = request.POST['celular']
+        correo = request.POST['correo']
+        user = persona(nombre=nombre, apellidoPaterno=apellidoPaterno, apellidoMaterno=apellidoMaterno,
+                               ciNumero=ciNumero, ciExtension=ciExtension, ciComplemento=ciComplemento, celular=celular,
+                               correo=correo)
+        user.save()
+        # currentUser = persona(nombre, apellidoPaterno, apellidoMaterno, ciNumero, ciExtension, ciComplemento, celular, correo).publish()
+        # currentUser.publish()
+        print(user.get_id_persona(), ' funciona')
+        response = render(request, 'RegisterClient.html', {'current_id': user.get_id_persona()})
+        response.set_cookie('id_persona', user.get_id_persona())
+        return response
+    else:
+        return render(request, 'RegisterPerson.html')
+
+def registroPersona(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        apellidoPaterno = request.POST['appP']
+        apellidoMaterno = request.POST['appM']
+        ciNumero = request.POST['CiN']
+        ciExtension = request.POST['CiE']
+        ciComplemento = request.POST['ciC']
+        celular = request.POST['celular']
+        correo = request.POST['correo']
+        user = persona(nombre=nombre, apellidoPaterno=apellidoPaterno, apellidoMaterno=apellidoMaterno,
+                               ciNumero=ciNumero, ciExtension=ciExtension, ciComplemento=ciComplemento, celular=celular,
+                               correo=correo)
+        user.save()
+        # currentUser = persona(nombre, apellidoPaterno, apellidoMaterno, ciNumero, ciExtension, ciComplemento, celular, correo).publish()
+        # currentUser.publish()
+        print(user.get_id_persona(), ' funciona')
+
+        #client = cliente(id_persona=persona(23), usuario='user01', contraseña='pass01')
+        #client.save()
+
+        response = render(request, 'RegisterClient.html', {'current_id': user.get_id_persona()})
+        response.set_cookie('id_persona', user.get_id_persona())
+        return response
+    else:
+        return render(request, 'RegisterPerson.html')
+
+def registroCliente(request):
+    if request.method == 'POST':
+        id_persona = request.COOKIES['id_persona']
+        usuario = request.POST['user']
+        contraseña = request.POST['pass']
+
+        client = cliente(id_persona=persona(id_persona), usuario=usuario, contraseña=contraseña)
+        client.save()
+
+        return render(request, 'ClientPanel.html')
